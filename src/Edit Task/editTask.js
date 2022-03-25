73e7861aea06a10bfa2230901c96db2f0e34c059
Swapnali <swapnalideshmukh189@gmail.com>
2022-03-25 17:22:43 +0530

    Initial Commit

---
+++ src/Edit Task/editTask.js
@@ -0,0 +1,35 @@
+// import styles from './editTask.module.css';
+// import { useState } from 'react';
+
+
+
+function EditTask(props){
+
+    // const [enteredTask,setEnteredTask]=useState('');
+    // function editTaskHandler(event) 
+    // {
+    //     event.preventDefault();
+    //     console.log(enteredTask)
+    //     props.onEditTask(enteredTask);
+    //     // setEnteredTask('');
+    // }
+    // function taskChangeHandler(event){
+    //     setEnteredTask(event.target.value);
+    // }
+
+
+
+    return(
+        // console.log("Hello")
+        <h1>Hello Edit here</h1>
+    //     <div className={styles.modal}>
+    //     <form onSubmit={editTaskHandler}>
+    //      <input type="text" id="task" className={styles.input} onChange={taskChangeHandler}/>
+    //      <button type="submit" className={styles.button}>Edit</button>
+    //     </form>
+ 
+    //  </div>
+    );
+}
+
+export default EditTask;
